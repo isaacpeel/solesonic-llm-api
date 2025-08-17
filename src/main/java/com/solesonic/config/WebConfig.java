e1ed2242ac60a5eed9a1ca626760f0f35418cd86
isaac <isaac@gmail.com>
2025-08-16 19:18:31 -0600

    Add logging for CORS configuration in `WebConfig`

--- src/main/java/com/solesonic/config/WebConfig.java
+++ src/main/java/com/solesonic/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.solesonic.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +14,15 @@ import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 public class WebConfig {
+    private static final Logger log = LoggerFactory.getLogger(WebConfig.class);
+
     @Value("${cors.allowed.origins}")
     private String[] allowedOrigins;
 
     @Bean
     public WebMvcConfigurer webConfigurer() {
+        log.info("Configuring CORS with allowed origins: {}", String.join(", ", allowedOrigins));
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
