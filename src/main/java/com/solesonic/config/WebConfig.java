e724a18ef5718dffee507d6e4d2991ff8536d57b
isaac <isaac@gmail.com>
2025-08-16 21:20:07 -0600

    Update `SecurityConfig` to fix formatting and ensure proper authorization configuration

--- src/main/java/com/solesonic/config/WebConfig.java
+++ src/main/java/com/solesonic/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import static org.springframework.http.HttpMethod.*;
 
 @Configuration
-@Profile("!prod")
 public class WebConfig {
     private static final Logger log = LoggerFactory.getLogger(WebConfig.class);
 
